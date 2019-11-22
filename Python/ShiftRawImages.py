92c69293cc35b97e5360778a5e175ef9019395d3
Qiang Gao <qiang.gao@gmail.com>
2019-11-21 17:24:15 -0800

    updated ShiftRawImages.py

--- Python/ShiftRawImages.py
+++ Python/ShiftRawImages.py
@@ -3,33 +3,56 @@ from __future__ import print_function
 
 import numpy as np
 
-def TranslateRawImage(in_raw_file, w, h, vert_mv, horizon_mv, out_raw_file):
-    in_raw = np.fromfile(in_raw_file, dtype=np.uint16)
+def TranslateRawImage(in_raw_file, w, h, horizon_mv, vert_mv, out_raw_file):
+    try:
+        in_raw = np.fromfile(in_raw_file, dtype=np.uint16)
+    except Exception as e:
+        print(e)
+        return -1
+
     in_raw = in_raw.reshape((h, w))  # (y, x)
 
-    out_raw = np.zeros((h, w))
+    out_raw = np.zeros((h, w), dtype=np.uint16) # + 8000
 
-    intersect_x = np.maximum(0, horizon_mv)
-    intersect_y = np.maximum(0, vert_mv)
+    if horizon_mv >= 0:
+        if vert_mv >= 0:
+            out_raw[vert_mv:, horizon_mv:] = in_raw[0: -vert_mv, 0: -horizon_mv ]
+        else:
+            out_raw[0:vert_mv, horizon_mv:] = in_raw[-vert_mv:, 0: -horizon_mv]
+    else:
+        if vert_mv >= 0:
+            out_raw[vert_mv:, 0:horizon_mv] = in_raw[0: -vert_mv, -horizon_mv:]
+        else:
+            out_raw[0:vert_mv, 0:horizon_mv] = in_raw[-vert_mv:, -horizon_mv:]
 
+    try:
+        out_raw.tofile(out_raw_file)
+    except Exception as e:
+        print(e)
+        return -2
+
+    return 0
 
 def main(args):
     import glob
-    in_raw_files = []
+    import os.path
+
     for p in args.in_raw_files:
         for f in glob.glob(p):
-            in_raw_files.append(f)
-
-
-
+            b, ext = os.path.splitext(f)
+            outf = '{}_shifted{}'.format(b, ext)
+            horizon_mv = np.random.randint(args.lower_motion_limit, args.upper_motion_limit)
+            vert_mv = np.random.randint(args.lower_motion_limit, args.upper_motion_limit)
+            print('{} => {}: {} {}'.format(f, outf, horizon_mv, vert_mv))
+            TranslateRawImage(f, args.width, args.tall, horizon_mv, vert_mv, outf)
 
 if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument('-x', type=int, help="max abs translation on x/horizontal direction - should be even for Bayer")
-    ap.add_argument('-y', type=int, help="max abs translation on y/vertical direction - should be even for Bayer")
-    ap.add_argument('-w', type=int, help="image width")
-    ap.add_argument('-t', type=int, help="image tall/height")
+    ap.add_argument('-l', '--lower_motion_limit', type=int, help="max abs translation - should be even for Bayer")
+    ap.add_argument('-u', '--upper_motion_limit', type=int, help="min abs translation - should be even for Bayer")
+    ap.add_argument('-w', '--width', type=int, help="image width")
+    ap.add_argument('-t', '--tall', type=int, help="image tall/height")
     ap.add_argument('in_raw_files', nargs='+', help="input raw files")
 
     args = ap.parse_args()
